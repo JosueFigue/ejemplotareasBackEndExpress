68077929bc93ac75cc457a6be3730fd187452ffa
JosueFigue <82242150+JosueFigue@users.noreply.github.com>
2023-08-31 13:03:00 -0600

    prueba 1 hola mundo

---
+++ test/01holamundo.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+
+
+describe('01 prueba hola mundo', () =>{
+    it('estoy haciendo la primer prueba', () => {
+        assert.equal(true, true)
+    });
+});
